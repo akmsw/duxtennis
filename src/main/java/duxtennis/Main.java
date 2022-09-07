a8d4f273a4e32cf7a9331cfb7d0e1bfad16f99c6
akmsw <franbonino82@gmail.com>
2022-09-07 13:37:24 -0300

    first commit

---
+++ src/main/java/duxtennis/Main.java
@@ -0,0 +1,29 @@
+package duxtennis;
+
+/**
+ * Main class, only for program initialization and useful fields declaration.
+ *
+ * @author Bonino, Francisco Ignacio.
+ * @version 0.0.1
+ * @since 07/09/2022
+ */
+public final class Main {
+
+  // ---------------------------------------- Constructor ---------------------------------------
+
+  /**
+   * Empty constructor.
+   */
+  private Main() {
+    // No body needed
+  }
+
+  /**
+   * Runs the program.
+   *
+   * @param args Program arguments (not used yet).
+   */
+  public static void main(String[] args) {
+    // TODO
+  }
+}
