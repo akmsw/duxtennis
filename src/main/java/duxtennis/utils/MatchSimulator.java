b9a94310f9bb93ccadc21333fd9754cc50b59db4
akmsw <franbonino82@gmail.com>
2022-09-09 02:12:12 -0300

    working on match simulation

--- src/main/java/duxtennis/utils/MatchSimulator.java
+++ src/main/java/duxtennis/utils/MatchSimulator.java
@@ -1,6 +1,10 @@
 package duxtennis.utils;
 
+import duxtennis.Main;
 import duxtennis.models.Match;
+import duxtennis.models.Views;
+import duxtennis.views.CurrentMatchView;
+import javax.swing.Timer;
 
 /**
  * Match simulator class.
@@ -24,6 +28,55 @@ public class MatchSimulator {
    * Starts the match simulation.
    */
   public void simulate() {
-    match.getPlayers().forEach(p -> System.out.println(p.getSkillPoints()));
+    Timer timer = new Timer(1000, null);
+
+    timer.addActionListener(e -> whoServes());
+    timer.setRepeats(true);
+    timer.start();
+  }
+
+  /**
+   * Bla.
+   */
+  private void whoServes() {
+    if (Math.random() <= (double) Main.getMatch()
+                                      .getPlayers()
+                                      .get(0)
+                                      .getSkillPoints() / 100) {
+      match.getPlayers()
+           .get(0)
+           .setServes(true);
+
+      match.getPlayers()
+           .get(1)
+           .setServes(false);
+
+      ((CurrentMatchView) Main.getController(Views.CURRENT_MATCH)
+                              .getView()).getTable()
+                                         .setValueAt("X", 1, 1);
+
+      ((CurrentMatchView) Main.getController(Views.CURRENT_MATCH)
+                              .getView()).getTable()
+                                         .setValueAt("", 2, 1);
+    } else {
+      match.getPlayers()
+           .get(0)
+           .setServes(false);
+
+      match.getPlayers()
+           .get(1)
+           .setServes(true);
+
+      ((CurrentMatchView) Main.getController(Views.CURRENT_MATCH)
+                              .getView()).getTable()
+                                         .setValueAt("", 1, 1);
+
+      ((CurrentMatchView) Main.getController(Views.CURRENT_MATCH)
+                              .getView()).getTable()
+                                         .setValueAt("X", 2, 1);
+    }
+
+    Main.getMatch().getPlayers().forEach(p -> System.out.println(p.getName() + "->" + p.serves()));
+    System.out.println("/////////////////////////////////////////////");
   }
 }
